ce431db87d4cd4ca6a765cc4b08bdb510d3f53f2
KevinTXY <tahakevin@gmail.com>
2017-10-28 16:14:18 -0500

    Added Sensitivity Filter to settings

--- src/settings.js
+++ src/settings.js
@@ -1,9 +1,11 @@
 function save_options(){
   var epichoice = document.getElementById('epi').value;
   var cbchoice = document.getElementById('cb').value;
+  var iichoice = document.getElementById('ii').value;
   chrome.storage.sync.set({
     myepichoice: epichoice,
-    mycbchoice: cbchoice
+    mycbchoice: cbchoice,
+    myiichoice: iichoice
   }, function(){
     var status = document.getElementById('status');
     status.textContent = 'Options saved.';
@@ -16,10 +18,12 @@ function save_options(){
 function restore_options(){
   chrome.storage.sync.get({
     myepichoice: 'disabled',
-    mycbchoice: 'disabled'
+    mycbchoice: 'disabled',
+    myiichoice: ''
   }, function(items){
   document.getElementById('epi').value = items.myepichoice;
   document.getElementById('cb').value = items.mycbchoice;
+  document.getElementById('ii').value = items.myiichoice;
   });
 }
 
