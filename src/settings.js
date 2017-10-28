5f0bf0724d04fc20552bef11fc9c4adcccd1802b
KevinTXY <tahakevin@gmail.com>
2017-10-28 02:07:27 -0500

    Save functionality working

--- src/settings.js
+++ src/settings.js
@@ -11,9 +11,14 @@ function save_options(){
 });
 }
 
+function restore_options(){
+  chrome.storage.sync.get({
+    mychoice: 'disabled'
+  }, function(items){
+  document.getElementById('epi').value = items.mychoice;
+  });
+}
 
 
-
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', restore_options);
 document.getElementById('save').addEventListener('click', save_options);
-});
