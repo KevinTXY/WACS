13fc70560245352111f11f9d9a3f25b6982e425e
RyanDelap <akayungeagle@gmail.com>
2017-10-29 03:24:59 -0500

    Add image reader support and blocker for questionable images

--- src/libs/anti-gif.js
+++ src/libs/anti-gif.js
@@ -1,34 +1,36 @@
-$('video').each(function(){
-    $(this).get(0).pause();
-    $(this).removeAttr("autoplay");
-});
 
-$('video').mouseenter(function(){
-    console.log("Hello World!");
-    $(this).get(0).play();
-})
-
-$('video').mouseleave(function(){
-    $(this).get(0).pause();
-})
-
-$('video').on('load', function () {
-    $(this).get(0).pause();
-});
-
-$(document).on('DOMNodeInserted', function (e) {
-    $('video', e.target).each(function(){
+function run() {
+    $('video').each(function(){
         $(this).get(0).pause();
         $(this).removeAttr("autoplay");
-
-        $('video').mouseenter(function(){
-            console.log("Hello World!");
-            $(this).get(0).play();
-        })
-
-        $('video').mouseleave(function(){
-            $(this).get(0).pause();
-        })
-
     });
-});
+
+    $('video').mouseenter(function(){
+        $(this).get(0).play();
+    })
+
+    $('video').mouseleave(function(){
+        $(this).get(0).pause();
+    })
+
+    $('video').on('load', function () {
+        $(this).get(0).pause();
+    });
+
+    $(document).on('DOMNodeInserted', function (e) {
+        $('video', e.target).each(function(){
+            $(this).get(0).pause();
+            $(this).removeAttr("autoplay");
+
+            $('video').mouseenter(function(){
+                console.log("Hello World!");
+                $(this).get(0).play();
+            })
+
+            $('video').mouseleave(function(){
+                $(this).get(0).pause();
+            })
+
+        });
+    });
+}
--- src/libs/textblur.js
+++ src/libs/textblur.js
@@ -1,5 +1,5 @@
-$( document ).ready(function() {
 
+function run() {
     $('p:contains("gun")').each(function(){
         $(this).css({"color": "transparent", "text-shadow": "0 0 30px rgba(0,0,0,1)"});
     });
@@ -35,84 +35,84 @@ $( document ).ready(function() {
     $('a:contains(rifle)').each(function(){
         $(this).css({"color": "transparent", "text-shadow": "0 0 30px rgba(0,0,0,1)"});
     });
-});
 
-$('p:contains(gun)').mouseenter(function(){
-    $(this).css({"color": "black", "text-shadow": "none"});
-})
+    $('p:contains(gun)').mouseenter(function(){
+        $(this).css({"color": "black", "text-shadow": "none"});
+    })
 
-$('p:contains(gun)').mouseleave(function(){
-    $(this).css({"color": "transparent", "text-shadow": "0 0 30px rgba(0,0,0,1)"});
-})
+    $('p:contains(gun)').mouseleave(function(){
+        $(this).css({"color": "transparent", "text-shadow": "0 0 30px rgba(0,0,0,1)"});
+    })
 
-$('a:contains(gun)').mouseenter(function(){
-    $(this).css({"color": "black", "text-shadow": "none"});
-})
+    $('a:contains(gun)').mouseenter(function(){
+        $(this).css({"color": "black", "text-shadow": "none"});
+    })
 
-$('a:contains(gun)').mouseleave(function(){
-    $(this).css({"color": "transparent", "text-shadow": "0 0 30px rgba(0,0,0,1)"});
-})
+    $('a:contains(gun)').mouseleave(function(){
+        $(this).css({"color": "transparent", "text-shadow": "0 0 30px rgba(0,0,0,1)"});
+    })
 
-$('p:contains(knife)').mouseenter(function(){
-    $(this).css({"color": "black", "text-shadow": "none"});
-})
+    $('p:contains(knife)').mouseenter(function(){
+        $(this).css({"color": "black", "text-shadow": "none"});
+    })
 
-$('p:contains(knife)').mouseleave(function(){
-    $(this).css({"color": "transparent", "text-shadow": "0 0 30px rgba(0,0,0,1)"});
-})
+    $('p:contains(knife)').mouseleave(function(){
+        $(this).css({"color": "transparent", "text-shadow": "0 0 30px rgba(0,0,0,1)"});
+    })
 
-$('a:contains(knife)').mouseenter(function(){
-    $(this).css({"color": "black", "text-shadow": "none"});
-})
+    $('a:contains(knife)').mouseenter(function(){
+        $(this).css({"color": "black", "text-shadow": "none"});
+    })
 
-$('a:contains(knife)').mouseleave(function(){
-    $(this).css({"color": "transparent", "text-shadow": "0 0 30px rgba(0,0,0,1)"});
-})
+    $('a:contains(knife)').mouseleave(function(){
+        $(this).css({"color": "transparent", "text-shadow": "0 0 30px rgba(0,0,0,1)"});
+    })
 
-$('p:contains(drug)').mouseenter(function(){
-    $(this).css({"color": "black", "text-shadow": "none"});
-})
+    $('p:contains(drug)').mouseenter(function(){
+        $(this).css({"color": "black", "text-shadow": "none"});
+    })
 
-$('p:contains(drug)').mouseleave(function(){
-    $(this).css({"color": "transparent", "text-shadow": "0 0 30px rgba(0,0,0,1)"});
-});
+    $('p:contains(drug)').mouseleave(function(){
+        $(this).css({"color": "transparent", "text-shadow": "0 0 30px rgba(0,0,0,1)"});
+    });
 
-$('a:contains(drug)').mouseenter(function(){
-    $(this).css({"color": "black", "text-shadow": "none"});
-})
+    $('a:contains(drug)').mouseenter(function(){
+        $(this).css({"color": "black", "text-shadow": "none"});
+    })
 
-$('a:contains(drug)').mouseleave(function(){
-    $(this).css({"color": "transparent", "text-shadow": "0 0 30px rgba(0,0,0,1)"});
-})
+    $('a:contains(drug)').mouseleave(function(){
+        $(this).css({"color": "transparent", "text-shadow": "0 0 30px rgba(0,0,0,1)"});
+    })
 
-$('p:contains(blood)').mouseenter(function(){
-    $(this).css({"color": "black", "text-shadow": "none"});
-})
+    $('p:contains(blood)').mouseenter(function(){
+        $(this).css({"color": "black", "text-shadow": "none"});
+    })
 
-$('p:contains(blood)').mouseleave(function(){
-    $(this).css({"color": "transparent", "text-shadow": "0 0 30px rgba(0,0,0,1)"});
-})
+    $('p:contains(blood)').mouseleave(function(){
+        $(this).css({"color": "transparent", "text-shadow": "0 0 30px rgba(0,0,0,1)"});
+    })
 
-$('a:contains(blood)').mouseenter(function(){
-    $(this).css({"color": "black", "text-shadow": "none"});
-})
+    $('a:contains(blood)').mouseenter(function(){
+        $(this).css({"color": "black", "text-shadow": "none"});
+    })
 
-$('a:contains(blood)').mouseleave(function(){
-    $(this).css({"color": "transparent", "text-shadow": "0 0 30px rgba(0,0,0,1)"});
-})
+    $('a:contains(blood)').mouseleave(function(){
+        $(this).css({"color": "transparent", "text-shadow": "0 0 30px rgba(0,0,0,1)"});
+    })
 
-$('p:contains(rifle)').mouseenter(function(){
-    $(this).css({"color": "black", "text-shadow": "none"});
-})
+    $('p:contains(rifle)').mouseenter(function(){
+        $(this).css({"color": "black", "text-shadow": "none"});
+    })
 
-$('p:contains(rifle)').mouseleave(function(){
-    $(this).css({"color": "transparent", "text-shadow": "0 0 30px rgba(0,0,0,1)"});
-})
+    $('p:contains(rifle)').mouseleave(function(){
+        $(this).css({"color": "transparent", "text-shadow": "0 0 30px rgba(0,0,0,1)"});
+    })
 
-$('a:contains(rifle)').mouseenter(function(){
-    $(this).css({"color": "black", "text-shadow": "none"});
-})
+    $('a:contains(rifle)').mouseenter(function(){
+        $(this).css({"color": "black", "text-shadow": "none"});
+    })
 
-$('a:contains(rifle)').mouseleave(function(){
-    $(this).css({"color": "transparent", "text-shadow": "0 0 30px rgba(0,0,0,1)"});
-})
+    $('a:contains(rifle)').mouseleave(function(){
+        $(this).css({"color": "transparent", "text-shadow": "0 0 30px rgba(0,0,0,1)"});
+    })
+}
