2fde8a024e9c232a2a238c68add5ef76d1d4ea4d
KevinTXY <tahakevin@gmail.com>
2017-10-28 20:50:55 -0500

    Updated settings page

--- src/settings.js
+++ src/settings.js
@@ -2,10 +2,12 @@ function save_options(){
   var epichoice = document.getElementById('epi').value;
   var cbchoice = document.getElementById('cb').value;
   var iichoice = document.getElementById('ii').value;
+  var cwchoice = document.getElementById('cw').value;
   chrome.storage.sync.set({
     myepichoice: epichoice,
     mycbchoice: cbchoice,
-    myiichoice: iichoice
+    myiichoice: iichoice,
+    mycwchoice: cwchoice
   }, function(){
     var status = document.getElementById('status');
     status.textContent = 'Options saved.';
@@ -19,11 +21,13 @@ function restore_options(){
   chrome.storage.sync.get({
     myepichoice: 'disabled',
     mycbchoice: 'disabled',
-    myiichoice: ''
+    myiichoice: '',
+    mycwchoice: 'disabled'
   }, function(items){
   document.getElementById('epi').value = items.myepichoice;
   document.getElementById('cb').value = items.mycbchoice;
   document.getElementById('ii').value = items.myiichoice;
+  document.getElementById('cw').value = items.mycwchoice;
   });
 }
 
