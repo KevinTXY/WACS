0e27322fa89a3dbb4e44e637c18ffd4211909729
RyanDelap <akayungeagle@gmail.com>
2017-10-28 21:06:21 -0500

    add textblur for hardcode LUL

--- src/libs/textblur.js
+++ src/libs/textblur.js
@@ -1,20 +1,117 @@
-var blocked_words = ["trump", "guns"];
+$( document ).ready(function() {
+    $('p:contains(gun)').each(function(){
+        $(this).css({"color": "transparent", "text-shadow": "0 0 30px rgba(0,0,0,1)"});
+    });
 
-$(document).ready(function(){
-  var all = document.getElementsByTagName("*");
+    $('a:contains(gun)').each(function(){
+        $(this).css({"color": "transparent", "text-shadow": "0 0 30px rgba(0,0,0,1)"});
+    });
+    $('p:contains(knife)').each(function(){
+        $(this).css({"color": "transparent", "text-shadow": "0 0 30px rgba(0,0,0,1)"});
+    });
 
-  for (var i=0, max=all.length; i < max; i++) {
-       var current = all[i];
-       for(word in blocked_words) {
-         setTimeout(test(current, blocked_words[word]), 1);
-       }
-  }
-  function test(current, word) {
-    if(current.textContent.indexOf(word) !== -1) {
-      console.log(current.textContent);
-      console.log(word)
-      current.style.color = "transparent";
-      current.style.textShadow = "0 0 30px rgba(0,0,0,1)";
-    }
-  }
+    $('a:contains(knife)').each(function(){
+        $(this).css({"color": "transparent", "text-shadow": "0 0 30px rgba(0,0,0,1)"});
+    });
+    $('p:contains(drug)').each(function(){
+        $(this).css({"color": "transparent", "text-shadow": "0 0 30px rgba(0,0,0,1)"});
+    });
+
+    $('a:contains(drug)').each(function(){
+        $(this).css({"color": "transparent", "text-shadow": "0 0 30px rgba(0,0,0,1)"});
+    });
+    $('p:contains(blood)').each(function(){
+        $(this).css({"color": "transparent", "text-shadow": "0 0 30px rgba(0,0,0,1)"});
+    });
+
+    $('a:contains(blood)').each(function(){
+        $(this).css({"color": "transparent", "text-shadow": "0 0 30px rgba(0,0,0,1)"});
+    });
+    $('p:contains(rifle)').each(function(){
+        $(this).css({"color": "transparent", "text-shadow": "0 0 30px rgba(0,0,0,1)"});
+    });
+
+    $('a:contains(rifle)').each(function(){
+        $(this).css({"color": "transparent", "text-shadow": "0 0 30px rgba(0,0,0,1)"});
+    });
 });
+
+$('p:contains(gun)').mouseenter(function(){
+    $(this).css({"color": "black", "text-shadow": "none"});
+})
+
+$('p:contains(gun)').mouseleave(function(){
+    $(this).css({"color": "transparent", "text-shadow": "0 0 30px rgba(0,0,0,1)"});
+})
+
+$('a:contains(gun)').mouseenter(function(){
+    $(this).css({"color": "black", "text-shadow": "none"});
+})
+
+$('a:contains(gun)').mouseleave(function(){
+    $(this).css({"color": "transparent", "text-shadow": "0 0 30px rgba(0,0,0,1)"});
+})
+
+$('p:contains(knife)').mouseenter(function(){
+    $(this).css({"color": "black", "text-shadow": "none"});
+})
+
+$('p:contains(knife)').mouseleave(function(){
+    $(this).css({"color": "transparent", "text-shadow": "0 0 30px rgba(0,0,0,1)"});
+})
+
+$('a:contains(knife)').mouseenter(function(){
+    $(this).css({"color": "black", "text-shadow": "none"});
+})
+
+$('a:contains(knife)').mouseleave(function(){
+    $(this).css({"color": "transparent", "text-shadow": "0 0 30px rgba(0,0,0,1)"});
+})
+
+$('p:contains(drug)').mouseenter(function(){
+    $(this).css({"color": "black", "text-shadow": "none"});
+})
+
+$('p:contains(drug)').mouseleave(function(){
+    $(this).css({"color": "transparent", "text-shadow": "0 0 30px rgba(0,0,0,1)"});
+});
+
+$('a:contains(drug)').mouseenter(function(){
+    $(this).css({"color": "black", "text-shadow": "none"});
+})
+
+$('a:contains(drug)').mouseleave(function(){
+    $(this).css({"color": "transparent", "text-shadow": "0 0 30px rgba(0,0,0,1)"});
+})
+
+$('p:contains(blood)').mouseenter(function(){
+    $(this).css({"color": "black", "text-shadow": "none"});
+})
+
+$('p:contains(blood)').mouseleave(function(){
+    $(this).css({"color": "transparent", "text-shadow": "0 0 30px rgba(0,0,0,1)"});
+})
+
+$('a:contains(blood)').mouseenter(function(){
+    $(this).css({"color": "black", "text-shadow": "none"});
+})
+
+$('a:contains(blood)').mouseleave(function(){
+    $(this).css({"color": "transparent", "text-shadow": "0 0 30px rgba(0,0,0,1)"});
+})
+
+$('p:contains(rifle)').mouseenter(function(){
+    $(this).css({"color": "black", "text-shadow": "none"});
+})
+
+$('p:contains(rifle)').mouseleave(function(){
+    $(this).css({"color": "transparent", "text-shadow": "0 0 30px rgba(0,0,0,1)"});
+})
+
+$('a:contains(rifle)').mouseenter(function(){
+    $(this).css({"color": "black", "text-shadow": "none"});
+})
+
+$('a:contains(rifle)').mouseleave(function(){
+    $(this).css({"color": "transparent", "text-shadow": "0 0 30px rgba(0,0,0,1)"});
+})
